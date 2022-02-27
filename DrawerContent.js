9dfa5a34520f86833a0bf6508e7ee968b1920f65
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-02-27 20:55:29 +0530

    Add files via upload

---
+++ DrawerContent.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native-web";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+import React from "react";
+
+import { Avatar, Tittle, Caption, Paragraph, Text, Drawer, TouchableRipple, Switch } from '@react-native-paper'
+
+
+export function DrawerContent(params) {
+    return (
+        
+    )
+}
